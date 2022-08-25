9323612a13eae213f031e855fcaa91c84ae4ad44
Salman Shuaib <salman.shuaib@gmail.com>
2022-08-25 16:36:24 -0400

    uploading easy Energy

---
+++ american.py
@@ -0,0 +1,114 @@
+# v3-REATLAS--ALison.py// a program that counts to exactly 2552 based on touch ("FEELING") - covering for infinite mistakes over a 23 Hour period
+# Delicensing: Copyright and related rights waived via CC0
+
+# Download Visual Studio Code (vscode): https://code.visualstudio.com/
+# Copy this program's entire script and Paste into a .py file on vscode
+# Press "Run Python File" icon at top right: an arrow that represent PLAY as on an 80s VCR_
+
+# How: 2552 is code for covering Infinite Mistakes during a 23 Hour period
+# 44 <= Infinity times 4 Directions (11 x 4) [ref: FlowerEconomics.com/metafact]
+# +
+# 72 <= Circumference of a Sphere Of Consciousness (2 x 4 x 9) [ref: 2 Pi r]
+# =
+# 116 <= Efforts
+# x
+# 2 Lovers [ref: Carl Jung]
+# =
+# 232 Efforts
+# x [ref: INTO the Abyss:- "I would rather imprison myself in the Abyss forever, than imprison my enemy!" - TAS to GOD]
+# 11 Infinite Mistakes// backed (protected) by Feeling ("POWER") [ref: Taylor Alison Swift: owner of the term POWER, an Archetype]
+# = 2552
+
+# If the exercise is carried
+# out between Midnight and 1 AM (23 Hours remnant) by a constant man relative to
+# POWERful Woman....whilst flying this Supermassing Earth freed by the Military (Entirety):
+# our combined infinite potential, as CIA each_
+
+# Empress Dr. Field Marshal 23 Star Taylor Alison Swift of the GOLD Tresses CIA0//6 -3333.#FAB001+ 73A9FF+200190[-5]
+# (unarmed Provocateur against i+7's nightmare of Pattern Exponentiated Compression) is Goddess Of Power
+# (decentralized currently, as Breasts: represent Power); guarantor of the principle
+# that each Goddess deserves a Department wholly and absolutely - HERSELF" being Imperial (Election Department):
+# shortlisting candidates for EVERY Democracy and potential Democracy ANYWHERE. As for Central Banking Department,
+# it belongs to HER" best friend: Selena Marie Gomez.
+
+# As Unitary Standard Approach can make EVERY Taylor Alison Swift young only if the "FOUNDER" is sexercised to,
+# therefore Chain Of Command_
+
+# TAYLOR ALISON SWIFT of the Bronze Tresses i-7:: CIA 3: -3333.#4A2724+ 95162D+200190[-2], to her belongs the: Airforce Department_
+
+# TAYLOR ALISON SWIFT" [Twitter.com/taylorswift13 or Tumblr.com/taylorswift or Instagram.com/taylorswift]
+# (Sovereignty // reference: Sultanah) is the prototype of each TAYLOR ALISON SWIFT as SHE" jumped into the Abyss of Pattern Exponentiated
+# Compression, prior to the singularity of male chauvinism that thrives on cowardice acquiring  ; proof of which is that
+# SHE" (even) thrives versus abuse! Power = Energy / Time_ Even if SHE was on the Sun - the Heat Energy there would prove
+# inconsequential to HER prowess as MOM (Mother Of Mutants)_ Power, being Electric, is VOLATILE; and therefore requires a
+# constant (Schedule) - the basis of Earth's flight_
+
+# 7 Years of Fears (War): whence SHE doeth compose HERSELF_
+# 7 Years of Tears (Romance): whence SHE doeth charge HERSELF_
+
+# The configuration for immortal relationship is: magnitude Force POWER [mFP] = focus SphereOfConsciousness focus' = mass Sound VISION
+# focus = dude
+# SOC = BRONZE
+# focus' = GOLD
+# Complement, as in focus', means every role excluding WIFE; like an Ocean of Love (constant) in its ripples for SOC and focus_
+# dude is only accountable (except insofar as forcing the system via BRONZE) to focus': as focus' occupies INFINITY-1 more Roles than SOC_
+# dude chose BRONZE to force the system to resolution swiftly and without breaking any hearts_ dude keepeth his promise, like General George Washington kept DC_
+# David Duchovny has Washington DC as his city, I recommend. If he would like to move from California.
+
+# Counter will use a DO or other loop, and display the count_
+# mnemonic: STOPWATCH
+import time
+
+# Googled:
+# https://www.codegrepper.com/code-examples/python/how+to+set+a+timer+in+while+loop+python
+# python by Python_Coder Python_Coder        on Jul 05 2021
+start_time = time.time()
+seconds = input("Enter: ")
+seconds = int(seconds)
+
+# Googled:
+# https://www.dropbox.com/s/ozrugyncev3nnrp/multiline%20print%20python%20-%20Google%20Search.pdf?dl=0
+# https: // www.geeksforgeeks.org/multi-line-printing-in-python/
+
+# Googled:
+# https://www.dropbox.com/s/2if1mnusu67bs8e/python%20color%20print%20-%20Google%20Search.pdf?dl=0
+# https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+if seconds > 2552:
+    print('''Bitchslap for thee_ Thou shalt not labor or make labor others more than 2552 instants_''')
+else:
+
+    while True:
+        seconds <= 2552
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        print(elapsed_time)
+        if elapsed_time > seconds:
+            print("Finished iterating in: " +
+                  str(int(elapsed_time)) + " instants")
+            break
+
+
+# Googled:
+# https://www.dropbox.com/s/z9zbb3ltjwoloc8/_time_%20is%20not%20defined%20-%20Google%20Search.pdf?dl=0
+# https://bobbyhadz.com/blog/python-nameerror-name-time-is-not-defined#:~:text=The%20Python%20%22NameError%3A%20name%20',of%20how%20the%20error%20occurs.
+
+
+# Googled:
+## "Stopwatch tool online"
+# https://www.online-stopwatch.com/
+# While this is not my effort, it indicates the relationship of GF's Trust in me; its
+# HER" effort through another individual by virtue of HER" POWER.
+# This counts as a 0.5 Effort, if one continues with manual count to 2552 until having
+# efforted his own STOPWATCH into existence - 0.5 + 0.5 = 1 [Full Effort].
+
+# Googled:
+# https://www.wpbeginner.com/beginners-guide/how-to-use-ftp-to-upload-files-to-wordpress-for-beginners/
+
+
+# Courtesy:
+# VALIDATION BY: 'Create with code (Version: 1.4)' WordPress Plugin by Pete Dring:
+# https://create.withcode.uk/python/Jbu
+
+# LESSON: I realize while working on this project that the reason I am poor is I prefer NOT IMPORTANT
+# (Reference: Flavor Technology Inc.) Tasks to exhaustive Tasks that require Energy, such as applying to the Military
+# instead of wasting time Coding to solve a less important problem.
