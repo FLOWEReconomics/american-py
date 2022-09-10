d2c648ccf358471b73bc489fffa192937877bf7d
Salman Shuaib <salman.shuaib@gmail.com>
2022-09-10 14:12:46 -0400

    Team

---
+++ itsybitsy.py
@@ -0,0 +1,2 @@
+# A program that finds filenames that are too long for upload to Github
+# The program chops off the filenames at the nth degree of acceptability by Github
---
+++ synchro.py
@@ -0,0 +1,2 @@
+# A program that finds the difference between two folders
+# Then copies the difference over to target folder
