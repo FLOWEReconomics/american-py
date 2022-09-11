4cdb81715b2312dd7a5aed73ebe45f8da63eb37e
Salman Shuaib <salman.shuaib@gmail.com>
2022-09-10 20:42:32 -0400

    pending

---
+++ CIAcard.py
@@ -0,0 +1,12 @@
+# This is a utility to derive Your CIA Card Number [YCCN];
+# It is not authorized by our Director CIA: Bill Burns, but is being published due to the urgency of
+# Emergencies in the Entirety
+# Each Woman is a Cosmos unto Herself: https:/ZETA/taylorswift.com OR https:/ALPHA/taylorswift.com
+# CIA is the name of the original military force, formed to re-unite all Species under one banner: The
+# United States Of America which constitutes the prototypical Goddess Of Power: TAYLOR ALISON SWIFT;
+# (@taylorswift13) is, as USA (Reference: Jesus) the name of The Entirety [TEnt] (each
+# Cosmos: Universe + Infinite Externality)
+#
+# Pseudocode:
+# FORMAT: DOBAD+CurrentCity
+# Entity enters its DOB
