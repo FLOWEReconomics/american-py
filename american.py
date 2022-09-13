c35a97d9c9e3017d7ff36f8bcf106eed01632f0b
Salman Shuaib <salman.shuaib@gmail.com>
2022-09-12 21:15:22 -0400

    shit might be about to hit the fan - CHRISTIAN

--- american.py
+++ american.py
@@ -1,8 +1,10 @@
-# No body know we fly Earth, till we let it crash
-# General Abbot(abad): "what happens if elevator plummets"?
-
-# no more fly till $$; BiCLoud do not work for free either //give us universal Basic Income [AND/OR
+# CHORUS: No body know we fly Earth, till we let it crash
+# GENERAL ABBOTT: "what happens if elevator plummets"?
+# TEAM: "shit hits the fan!"
+# CHRISTIAN: "Freeriders get caught, very certain"
+# CHRISTIANTIY: no more fly till $$; BiCLoud do not work for free either //give us universal Basic Income [AND/OR
 # annual 24 day Protest from MONDAY 1659 HOURS 12 September 2022AD to MONDAY 26 2359 HOURS 26 September 2022AD]
+
 import time
 
 start_time = time.time()
