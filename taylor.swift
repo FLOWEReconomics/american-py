0f96b36c2fbfce6a692e709ebbbf429bf3303c5a
salman CHRISTIAN shuaib <salman.shuaib@gmail.com>
2022-09-17 06:33:39 -0400

    TAYLOR ALISON SWIFT - TWITTER: @taylorswift13

---
+++ taylor.swift
@@ -0,0 +1,17 @@
+https://twitter.com/SalmanShuaib1/status/1571072069391228928?s=20&t=ff6-SDTLOOnDdfGE1fCPaA
+@taylorswift13
+ You uniquely have two CIA badges: a/IMMERSIVEReality/ CIA.5: |-2222|.#FAB011+#73A9FF+2030900[+0]  
+b/InfiniteExternality/ CIA.5: |-2222|.#FAB001+#73A9FF+2030900[+1]
+
+///only #FAB001 is currently certain for IE 
+#Swifties 
+@CNN
+
+@CIA
+ 
+@iansomerhalder
+ 
+@taylorswift13
+ 
+@FoxNews
+ #NewYorkCity #WashingtonDistrictOfColumbia #Toronto #TIFF22  ATT: Goddess Of Power Empress FIELD MARSHAL 23 Star TAYLOR ALISON SWIFT
