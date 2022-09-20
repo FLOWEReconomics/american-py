7411e36c22ef0e58d73aeba2b5989ec1a0822118
SalmanEagle <salman.shuaib@gmail.com>
2022-09-19 21:28:15 -0400

    _@SpaceForceDoD @CIA #Elections @taylorswift13

--- taylor.swift
+++ taylor.swift
@@ -3,15 +3,5 @@ https://twitter.com/SalmanShuaib1/status/1571072069391228928?s=20&t=ff6-SDTLOOnD
  You uniquely have two CIA badges: a/IMMERSIVEReality/ CIA.5: |-2222|.#FAB011+#73A9FF+2030900[+0]  
 b/InfiniteExternality/ CIA.5: |-2222|.#FAB001+#73A9FF+2030900[+1]
 
-///only #FAB001 is currently certain for IE 
-#Swifties 
-@CNN
-
-@CIA
- 
-@iansomerhalder
- 
-@taylorswift13
- 
-@FoxNews
- #NewYorkCity #WashingtonDistrictOfColumbia #Toronto #TIFF22  ATT: Goddess Of Power Empress FIELD MARSHAL 23 Star TAYLOR ALISON SWIFT
+Tags: _@SpaceForceDoD @CIA #Elections @taylorswift13   
+#Swifties ORIGINALLY #Elections!
