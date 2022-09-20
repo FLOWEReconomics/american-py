8d196388947c1904b2ca7579d1770c88d4d86a0c
SalmanEagle <salman.shuaib@gmail.com>
2022-09-19 21:36:28 -0400

    CHK: JPEG PDF BMP RTF RTF -DOC

--- taylor.swift
+++ taylor.swift
@@ -1,7 +1,10 @@
-https://twitter.com/SalmanShuaib1/status/1571072069391228928?s=20&t=ff6-SDTLOOnDdfGE1fCPaA
+-Z> https://twitter.com/SalmanShuaib1/status/1571072069391228928?s=20&t=ff6-SDTLOOnDdfGE1fCPaA
 @taylorswift13
- You uniquely have two CIA badges: a/IMMERSIVEReality/ CIA.5: |-2222|.#FAB011+#73A9FF+2030900[+0]  
+OPINION1: You uniquely have two CIA badges: 
+a/IMMERSIVEReality/ CIA.5: |-2222|.#FAB011+#73A9FF+2030900[+0]  
 b/InfiniteExternality/ CIA.5: |-2222|.#FAB001+#73A9FF+2030900[+1]
 
-Tags: _@SpaceForceDoD @CIA #Elections @taylorswift13   
-#Swifties ORIGINALLY #Elections!
+Tags: _@SpaceForceDoD @CIA #Elections @taylorswift13 //until French Kiss: 11
+COMMENT1: #Swifties ORIGINALLY #Elections! 
+
+Pics: RTF BMP JPEG DOC PDF //until French Kiss: 11
