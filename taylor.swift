82ca41e31ec35677544a5dd89debd914b30d8228
SalmanEagle <salman.shuaib@gmail.com>
2022-09-21 10:20:20 -0400

    yah, fuk the patriarchy_ Easy A

--- taylor.swift
+++ taylor.swift
@@ -13,4 +13,4 @@ Pics: RTF BMP DOCX JPEG PDF
 
 REFERENCE1: Zeus of Hera!_ [MEDITATION_]_
 REFERENCE2: MOM (Mother Of Mutants) [MEDITATION_] 
-@taylorswift13                     
+@taylorswift13                      
