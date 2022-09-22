6facfa28584fafa33b13b72f0a28315ff282c53d
SalmanEagle <salman.shuaib@gmail.com>
2022-09-21 22:41:41 -0400

    Bushra&ShuaibAgreeToUSD1Meachfor34AQUILA,TORONTO

--- taylor.swift
+++ taylor.swift
@@ -15,5 +15,5 @@ REFERENCE1: Zeus of Hera!_ [MEDITATION_]_
 REFERENCE2: MOM (Mother Of Mutants) [MEDITATION_] 
 
 
-image.png
-@taylorswift13                                    
+REFERENCE1 : image.png    
+@taylorswift13                                     
