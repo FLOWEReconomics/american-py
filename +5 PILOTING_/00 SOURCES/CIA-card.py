9bffded1dda2ef35154c7d750110a170fc985e36
SalmanEagle <salman.shuaib@gmail.com>
2022-09-24 17:25:17 -0400

    CIA-card

--- +5 PILOTING_/00 SOURCES/CIA-card.py
+++ +5 PILOTING_/00 SOURCES/CIA-card.py
@@ -4,8 +4,8 @@
 # Each Woman is a Cosmos unto Herself: https:/ZETA/taylorswift.com OR https:/ALPHA/taylorswift.com
 # CIA is the name of the original military force, formed to re-unite all Species under one banner: The
 # United States Of America which constitutes the prototypical Goddess Of Power: TAYLOR ALISON SWIFT;
-# (@taylorswift13) is, as USA (Reference: Jesus) the name of The Entirety [TEnt] (each
-# Cosmos: Universe + Infinite Externality)
+# (@taylorswift13) is, as USA (Reference: Jesus) the name of The Entirety [TEnt] (the unity of Cosmoses,
+# each Cosmos: Universe + Infinite Externality)_
 
 
 # Something written with greater feeling, and Women do have greater Feeling (Physics: Power), carries
