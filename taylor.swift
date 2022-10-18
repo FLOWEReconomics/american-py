6a9f49ffa3c9c22cf71db32f661426f7d8fddfe8
salman CHRISTIAN Shuaib <salman.shuaib@GMAIL.Com>
2022-10-18 18:07:05 +0800

    1

--- taylor.swift
+++ taylor.swift
@@ -1,5 +1,5 @@
 +0 Allied Goal: raise DXY //https://www.marketwatch.com/investing/index/dxy
-COMMENT1: Inflation symptom of DXY rising //Reason Inflation going high - will rise to 100% per annum (98% Interest Rate per annum) to achieve Net 2% Inflation /annum: 
+COMMENT1: Inflation symptom of DXY Rising //Reason Inflation going high - will rise to 100% per annum (98% Interest Rate per annum) to achieve Net 2% Inflation /annum: 
 + USD rises by purchase of USD (ESPECIALLY by Zero Net Worth Individuals due to Personal Leverage Factor differential relative Elon Musk)
 ++ Vicki Wahlstrom experiment on YouTube.com: Pop Can Implosion Experiment //demonstrates no vacuum: Money and Power are complements 
 + Purchasing Power Parity (PPP) increases for each American_
