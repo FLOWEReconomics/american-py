ddb376839da0697af6e23c8d749631885409c7dc
SalmanEagle <salman.shuaib@gmail.com>
2022-11-06 12:54:44 -0500

    Dr. Swift, i made a minor error //there is a typo in preceding commit message: seeks should be sees

---
+++ +5 PILOTING_/Plans/BATTLEground2022AD.swift
@@ -0,0 +1,6 @@
+//masculine masturbation addiction?: Lurasidone OR Risperidone (Risperdal) OR Divalproex Sodium 
+E.C 1000 MG PO DAILY SCH OR Perphenazine 8 MG PO NIGHLY SCH OR ARIPiprazole 10 MG PO DAILY SCH --assumption: 
+all Singularities are now equal, otherwise combination: brings under CONTROL- at cost of premature 
+ejaculation  //Dr. Swift: the conservative wave on Earth is capitalizing on the following (present) risk to 
+your prevalence:- man watching hardcore porn, even heterosexual, results in man gaining traction towards 
+(attraction to) penis [man seeks girl, but SEES girl enjoying penis, man starts enjoying penis]
