84342e490e7e54077fd1ab63761a5c7de9bc1551
SalmanEagle <salman.shuaib@gmail.com>
2022-11-06 18:49:10 -0500

    we do no surrender_

--- +5 PILOTING_/Plans/BATTLE2022AD.swift
+++ +5 PILOTING_/Plans/BATTLE2022AD.swift
@@ -1,7 +1,8 @@
 -> https://whatmyagenow.com/what-day-was-i-born  
--> https://www.270towin.com/2022-governor-election/  //pay attention dearest to the 2022 Column (candidates)
+-> https://www.270towin.com/2022-senate-election/ //pay attention dears to this list of 2022AD Senate candidates.  
+-> https://www.270towin.com/2022-governor-election/  //pay attention dears to the 2022AD Column (candidates)_
 -> https://edition.cnn.com/interactive/2022/10/politics/key-states-races-2022/index.html //3LVLs in SIX States
-^-> SETUP //there IS a path to Victory: defeating Republicans in 2022AD: Teamwork
+^-> SETUP //SHE May Still Make A Way: defeating Republicans in 2022AD: Teamwork //baton pass
 
 
 
