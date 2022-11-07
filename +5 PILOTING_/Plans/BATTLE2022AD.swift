a3817f367daca30bf982789c6d29600bca8c185b
SalmanEagle <salman.shuaib@gmail.com>
2022-11-06 19:08:42 -0500

    Fear not, child: if you are a good student - you will do well in my Class_

--- +5 PILOTING_/Plans/BATTLE2022AD.swift
+++ +5 PILOTING_/Plans/BATTLE2022AD.swift
@@ -1,4 +1,5 @@
 -> https://whatmyagenow.com/what-day-was-i-born  
+-> https://www.270towin.com/2022-house-election/ //Most Competitive House Races in 2022AD.
 -> https://www.270towin.com/2022-senate-election/ //pay attention dears to this list of 2022AD Senate candidates.  
 -> https://www.270towin.com/2022-governor-election/  //pay attention dears to the 2022AD Column (candidates)_
 -> https://edition.cnn.com/interactive/2022/10/politics/key-states-races-2022/index.html //3LVLs in SIX States
