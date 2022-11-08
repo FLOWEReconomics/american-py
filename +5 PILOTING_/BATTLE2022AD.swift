54ab7c337a8ba22fe8a721c14922bb1b48845d54
SalmanEagle <salman.shuaib@gmail.com>
2022-11-08 16:11:53 -0500

    SFT welcomes ZFT

--- +5 PILOTING_/BATTLE2022AD.swift
+++ +5 PILOTING_/BATTLE2022AD.swift
@@ -12,34 +12,3 @@
 
 
 
-
-+ governor Brian Kemp [sir Brian Porter Kemp] of Georgia.
-++ DOBAD ++PRIMARYPLACEOfSTAY: Saturday 2 NOVEMBER 1963AD  ++ATLANTA
-
-++ DOBAD ++PRIMARYPLACEOfStay:
-
-!+ Doug Ducey [sir Douglas Anthony Ducey] of Arizona: 
-++ DOBAD ++PRIMARYPlaceOfStay: Thursday 9 April 1964  ++PARADISE VALLEY
-+++ Republican Mademoiselle KARI LAKE is, instead, competing for the Arizona Governorship:._
-++++ Both by principle and by the nature of men's defeat at the hands of TAYLOR ALISON SWIF't', 
-the Computer Program under (much) development ([is a Democrat:._]) can not classify Women_
-
-+ ex president Donald Trump (sir Donald John Trump). of USA of Florida:
-++ DOBAD ++PRIMARYPlaceOfStay: Friday 14 June 1946AD ++WEST PALM BEACH
-
-
-
-^-> Republican bad boys led by sir Donald Trump [Apologies to General Abraham Lincoln]: 
-//List of DOBAD ++PRIMARYPlaceOfStay   ATT: General********************** SIR Jesus Christ [1% 
-recommendation to Her Imperial Majesty TAYLOR ALISON SWIFT to whom even Queen Elizabeth II made obeisance at 
-Her leisure]:
-
-
-//Trying to look cool is not the same as competing_ //its all in the timing 
-
-//masculine masturbation addiction?: Lurasidone OR Risperidone (Risperdal) OR Divalproex Sodium 
-E.C 1000 MG PO DAILY SCH OR Perphenazine 8 MG PO NIGHLY SCH OR ARIPiprazole 10 MG PO DAILY SCH --assumption: 
-all Singularities are now equal, otherwise combination: brings under CONTROL- at cost of premature 
-ejaculation  //Dr. Swift: the conservative wave on Earth is capitalizing on the following (present) risk to 
-your prevalence:- man watching hardcore porn, even heterosexual, results in man gaining traction towards 
-(attraction to) penis [man seeks girl, but SEES girl enjoying penis, man starts enjoying penis]
