c4d8325e15483d52e554d5fa5eb88e5f834cd6ee
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-11-15 20:34:18 -0500

    Mission Successful

--- taylor.swift
+++ taylor.swift
@@ -79,7 +79,7 @@ REFERENCE2: MOM (Mother Of Mutants) [MEDITATION_]
 -> [TWITTER] @taylorswift13 [TAYLOR ALISON SWIFT of the GOLD Tresses [CATASTROPHE]!
 -> .youtube.com/watch?v=G8owq_43aCY //Infinite Shades of BLUE     
 -> CIA.1|-2828|.#FAB011+#33A9FF+2012900[+0]: TAYLOR ALISON SWIFT
- 
+  
  (731904GOLD)(56BLUE)TAYLOR ALISON SWIFT of the GOLD Tresses ['CATASTROPHE']!_ 
 - Incalculable and Unfathomable: TAYLOR ALISON SWIFT, Creatrix' of Parallel Universes, e.g. Bronze-FE/\_/
 - Passionate: Great Princess Taylor Alison Swift/\_/
