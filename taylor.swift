cc67092dae713838007209572049a5454bffc62f
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-11-19 12:12:57 -0500

    Story, Love Stor'e'y

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acfd2dc15a7b3e71d6cf23476a1d143ff9ab3be6ebb4662a967b9ce0a10f5149
-size 4704
+oid sha256:ce4d2f6878efb450ff6ce5829d716e8d6560e6774eb8d53f447938cc955f31d8
+size 208
