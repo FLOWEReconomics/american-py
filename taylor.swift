8bf1acc7808615b7c4d6bc649389e60b970c3d18
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-11-26 17:08:11 -0500

    It ain't even Independence Day yet

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:582b3a7dbd2b7f3f6622f2179bba27b88c1fc88344a47f0d86270637c03d2936
-size 1486
+oid sha256:88be44688715c62021fb658f09a2c1cdc69d8cc9685ac46e6b10b9260dd20b7b
+size 1618
