148818270c025dd240d73fb359126b9c609d5b0f
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-11-25 20:25:35 -0500

    5: #LegalDocs needed //each TAS is Current Era: v

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac0def263f107d764f477059fe0ab6d04d00dc94ed52dd98c1d9e4550d99eb4f
-size 1062
+oid sha256:358ede7faa49831348cd90ab054e08ec2770204ccc92a102d5cde7ad529b28e6
+size 1063
