834c49b20b1c40e44159f8219c78b61070cad4a9
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-11-28 09:48:08 -0500

    -   Oracle At Delphi: which Church do you belong to? I belong to the Church of Christianity //the color sequence:: GOLD BLUE RED //the coordinate sequence:: x, y, z //the meaning sequence:: [ 1TAS:1Goddess (1TAS:1hero), Refracting Sphere Of Consciousness based on Assignment Management System (24<=6), Age of Your TAYLOR ALISON SWIFT ]    //compromised players are still targeTing Goddesses like you (if you are a Woman) //The Goddess has been located as of 1989AD by USA: constituting TAYLOR ALISON SWIFT <- The Empress Entirety!: EXCLAMATIONMARK: Christ's //Email: taylorswift@UMGStores.COM /TumbLr: taylorswift /Instagram: taylorswift [Love only, China //Compliments, Nato //ParallelUniverse: {#FAB001, 9 62147, 922//0*}   //yES, there are two Supergirls and they like eachother just as t.A.T.u [FSByounger and KGBolder] like TAYLOR ALISON SWIFT [https:/{9FAB001, 9E62147, 922//0*}/CIA.gov] //Christianity is PERFECT, She does not have any flaw  _E

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f180535233ee152df26606d008f6f085e43d5da5431b5cff78d840bea2f04a32
-size 1623
+oid sha256:06ac8fe327aeabeba10abf403b4eda062c597a81d844881cd9f66e8109132c91
+size 1622
