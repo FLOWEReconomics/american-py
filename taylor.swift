8b73df067b017873a3b7822b39a0ec8db97e6db4
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-11-27 21:17:17 -0500

    Oracle Of Delphi: the color sequence:: GOLD BLUE RED //compromised players are still targeTing Goddesses like you (if you are a Woman) //The Goddess has been located as of 1989AD by USA: constituting TAYLOR ALISON SWIFT <- The Empress Entirety!: EXCLAMATIONMARK: Christ's //Email: taylorswift@UMGStores.COM /TumbLr: taylorswift /Instagram: taylorswift [Love only, China //Compliments, Nato //ParallelUniverse: {#GCAI0D, 9 62147, 9G6//0*}   //yES, there are two Supergirls and they like eachother just as t.A.T.u [FSByounger and KGBolder] like TAYLOR ALISON SWIFT [https:/{9GCAI0D, 9G62147, 9G6//0*}/CIA.gov]

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9275102a7bc27b6a039752d3cbef90f49910ccce70e544b32801d9b1b2afae9
-size 1622
+oid sha256:f590542d94e161bd9f4f42feb1c1b99ab04013b148f25dcec09c2d4012d24176
+size 1623
