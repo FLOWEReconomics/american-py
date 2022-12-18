661f51b916d96d3ebe5f81935a5219dff2932441
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-12-18 13:07:00 -0500

    ~$ = -I Queen among the Goddesses of Destruction, WAS cut in halves  #ThousandsCuts-

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:882d25fb4d4315f51287f6e8766574fcf2dacae3e6d66c417955d91b1a3aa071
-size 7871
+oid sha256:6f63ce268d36c9b48d98811face01789c4262339be6cf553878d9b74256f8cbc
+size 7937
