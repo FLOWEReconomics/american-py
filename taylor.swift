c2b7c9584e2c37ab5be15b07765e2e242f4ec508
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-12-19 18:00:35 -0500

    all decisions are made based on present

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb95f8c0e35722a809266e86c5ca7368a7ebafe82e6d5007f9e5c54bfc5ae06a
-size 8771
+oid sha256:ba078be59a9a95984ea06963fb2d6d1a8e40e36ec2f1ccfbd3207fa361c0ef91
+size 9656
