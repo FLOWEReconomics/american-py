e938c6dad66315c412e43dd40a13838c9418e79b
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-12-19 18:16:12 -0500

    take total risk when you are losing, take zero risk when you are not - the problem is SOLVED; otherwise !: zero risk:- infinite time to solve //determining easiest most standard path /// Audience: yes he has solved it before uniquely: producing MYRIAD ages 1RED to 50RED /// Credit Rating: Negative //Result: 2/3: salman can solve

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba078be59a9a95984ea06963fb2d6d1a8e40e36ec2f1ccfbd3207fa361c0ef91
-size 9656
+oid sha256:d6ac5a333d3232b5d24c16a7b1b50c0766c315e7b721b30f3c501b833af1f6bb
+size 9768
