1b2496b8a01ad5dc724ee55e40dc58b188d689b4
Salman C_ Shuaib <salman.shuaib@GMAIL.com>
2022-12-19 23:41:10 -0500

    Prevailing

--- taylor.swift
+++ taylor.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e06ace4510b320ea2f03e93c52418a5a2c4c0709324b9a44c7a10ec336252740
-size 9924
+oid sha256:4ca589a865f5397115c6a869a01ad27adad390863a0eb4d6d5aef4e6948670c9
+size 10368
